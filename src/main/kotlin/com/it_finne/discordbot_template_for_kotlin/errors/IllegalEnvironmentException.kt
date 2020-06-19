daf1cee2fbe9cf6ee83ef0091d793c8913dcc3bd
finne0528 <kazuki.mineshima@it-finne.com>
2020-06-19 23:02:08 +0900

    refactoring

--- src/main/kotlin/com/it_finne/discordbot_template_for_kotlin/errors/IllegalEnvironmentException.kt
+++ src/main/kotlin/com/it_finne/discordbot_template_for_kotlin/errors/IllegalEnvironmentException.kt
@@ -2,4 +2,6 @@ package com.it_finne.discordbot_template_for_kotlin.errors
 
 import java.lang.RuntimeException
 
-class IllegalEnvironmentException(errMsg: String) : RuntimeException(errMsg)
+class IllegalEnvironmentException(environmentValue: String) : RuntimeException(
+    "invalid environment: ${environmentValue}. please choice production, staging or development."
+)
