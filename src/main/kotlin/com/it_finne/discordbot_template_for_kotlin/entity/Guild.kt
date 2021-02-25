e61d095dae2872e01bdfc2cb7fe401dcd7d1f715
finne0528 <kazuki.mineshima@it-finne.com>
2021-02-25 10:44:11 +0900

    add created_at and updated_at column

--- src/main/kotlin/com/it_finne/discordbot_template_for_kotlin/entity/Guild.kt
+++ src/main/kotlin/com/it_finne/discordbot_template_for_kotlin/entity/Guild.kt
@@ -5,11 +5,15 @@ import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.`java-time`.datetime
+import java.time.LocalDateTime
 
 object GuildRecords: LongIdTable(name="guilds") {
     val guildId: Column<Long> = long("guild_id").uniqueIndex()
     val name: Column<String> = varchar("name", 255)
     val prefix: Column<String> = varchar("prefix", 255)
+    val createdAt: Column<LocalDateTime> = datetime("created_at").default(LocalDateTime.now())
+    val updatedAt: Column<LocalDateTime> = datetime("updated_at").default(LocalDateTime.now())
 
     override val primaryKey = PrimaryKey(id)
 }
@@ -20,4 +24,6 @@ class GuildRecord(id: EntityID<Long>): LongEntity(id) {
     var guildId: Long by GuildRecords.guildId
     var name: String by GuildRecords.name
     var prefix: String by GuildRecords.prefix
+    var createdAt: LocalDateTime by GuildRecords.createdAt
+    var updatedAt: LocalDateTime by GuildRecords.updatedAt
 }
